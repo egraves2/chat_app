20010a69c370430fc5016aa53f081bd36dfc76b0
Erin Graves <egraveshna16@gmail.com>
2020-01-17 09:04:37 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <main>
+
+    <header> 
+      <img className="menu"
+        alt="menu"
+        src="https://i.ibb.co/nRJWZFM/2044284-512.png"
+      />
+      <img className="logo"
+        alt="picture"
+        src="https://i.ibb.co/TrPw6G9/Char-Get-To-Know-Batman80-5ca54cb83a27a6-53173051.png"
+      />
+      Batman
+    </header>
+
+    <TextInput onSend={t=> console.log(t)}/>
+
+  </main>
+}
+
+function TextInput(props){
+  var [text, setText] = useState('') 
+  return <div className="text-input">
+    <input className="text-field" value={text}
+      placeholder ="write your message"
+      onChange={e=> setText(e.target.value)}
+    />
+    <button className="button" onClick={()=> {
+      
+      props.onSend(text)
+      setText('')
+    }}>
+      <img className="icon" src="https://cdn0.iconfinder.com/data/icons/superglyph-communication/30/message-sending-512.png" />
+    </button>
+  </div>
+}
+
+function PrintText(props){
+  
 }
 
 export default App;
